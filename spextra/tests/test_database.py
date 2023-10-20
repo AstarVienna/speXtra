6f5c1bfc202759ecba3f351347e726708d87f319
teutoburg <ghost@instruct.at>
2023-10-20 19:44:44 +0200

    Update tests for new structure

--- spextra/tests/test_database.py
+++ spextra/tests/test_database.py
@@ -3,15 +3,18 @@
 
 import urllib
 import pytest
+import yaml
 
-from spextra.database import DataContainer, Database, SpecLibrary
+from spextra.database import Database
+from spextra.libraries import SpecLibrary
 from spextra.configuration import config
 
 
 @pytest.fixture(scope="class", name="datacont")
-@pytest.mark.usefixtures("mock_dir")
-def simple_datacontainer(mock_dir):
-    return DataContainer(filename=mock_dir / "index.yml")
+def simple_yamldict(mock_dir):
+    path = mock_dir / "index.yml"
+    with path.open(encoding="utf-8") as file:
+        return yaml.safe_load(file)
 
 
 # @pytest.fixture(scope="module")
@@ -29,28 +32,27 @@ def mock_database():
 
 @pytest.fixture(scope="module")
 def url_result():
-    url = config.get_database_url()
+    url = config.database_url
     return urllib.parse.urlparse(url)
 
 
-@pytest.mark.usefixtures("url_result")
 @pytest.mark.parametrize("attribute", ["scheme", "netloc", "path"])
 def test_database_location(url_result, attribute):
     """test if a valid url"""
     assert getattr(url_result, attribute)
 
 
-class TestDataContainer:
-    @pytest.mark.usefixtures("datacont")
-    @pytest.mark.parametrize("attribute", ["libraries",
-                                           "extinction_curves",
-                                           "filter_systems"])
-    def test_attr_creation(self, datacont, attribute):
-        assert hasattr(datacont, attribute)
+class TestDatabase:
+    @pytest.mark.parametrize("key", ["libraries",
+                                     "extinction_curves",
+                                     "filter_systems"])
+    def test_keys(self, datacont, key):
+        assert key in datacont
+        assert len(datacont[key])
 
 
 class TestSpecLibrary:
-    @pytest.mark.parametrize("library_name", mock_database().libraries)
+    @pytest.mark.parametrize("library_name", mock_database()["libraries"])
     def test_name(self, library_name):
         lib = SpecLibrary(library_name)
         assert lib.name == library_name
@@ -58,9 +60,9 @@ class TestSpecLibrary:
     def test_templates(self):
         name = "kc96"
         lib = SpecLibrary(name)
-        assert "bulge" in lib.items.keys()
+        assert "bulge" in lib.keys()
 
-    @pytest.mark.parametrize("library_name", mock_database().libraries)
+    @pytest.mark.parametrize("library_name", mock_database()["libraries"])
     @pytest.mark.parametrize("attribute", ["file_extension", "data_type"])
     def test_attr(self, library_name, attribute):
         lib = SpecLibrary(library_name)
